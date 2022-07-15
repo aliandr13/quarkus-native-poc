7061e048a9b83bbdf700c9255d2708eb681d1bd4
Aliaksandr Zenkevich <Aliaksandr_Zenkevich@epam.com>
2022-07-16 00:33:55 +0200

    bump quarkus version 2.5.4

--- src/test/java/com/quarkus/poc/LambdaHandlerTest.java
+++ src/test/java/com/quarkus/poc/LambdaHandlerTest.java
@@ -7,10 +7,10 @@ import io.quarkus.test.junit.QuarkusTest;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 
-//@QuarkusTest
+@QuarkusTest
 public class LambdaHandlerTest {
 
-//    @Test
+    @Test
     public void testSimpleLambdaSuccess() throws Exception {
         // you test your lambdas by invoking on http://localhost:8081
         // this works in dev mode too
